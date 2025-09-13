621d1a566471ef34acbacf1f7fbbb2a08e4ddb85
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2025-09-13 11:38:02 -0300

    Styles enh

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -12,94 +12,72 @@ export default function Projects() {
           <VerticalTimeline>
               <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                contentStyle={{ background: '#1ccebf', color: '#000' }}
                 contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                date="2011 - present"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                date="2022 - present"
+                iconStyle={{ background: '#1ccebf', color: '#000' }}
                 icon={<>p</>}
               >
-                <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                <h3 className="vertical-timeline-element-title text-black font-bold text-2xl">Castine LLC 2022 - present</h3>
+                <h4 className="vertical-timeline-element-subtitle font-bold text-xl">Frontend Technical Lead</h4>
                 <p>
-                  Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                  Frontend Technical Lead, Project Management, Team Leading
                 </p>
               </VerticalTimelineElement>
               <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2010 - 2011"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                contentStyle={{ background: '#1ccebf', color: '#000' }}
+                date="2019 - 2022"
+                iconStyle={{ background: '#1ccebf', color: '#000' }}
                 icon={<>p</>}
               >
-                <h3 className="vertical-timeline-element-title">Art Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                      <h3 className="vertical-timeline-element-title text-black font-bold text-2xl">Mercado Libre 2019 - 2022</h3>
+                      <h4 className="vertical-timeline-element-subtitle font-bold text-xl">Frontend Technical Lead</h4>
                 <p>
-                  Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                Frontend Technical Lead, Project Management, Team Leading
                 </p>
               </VerticalTimelineElement>
               <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2008 - 2010"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                contentStyle={{ background: '#1ccebf', color: '#000' }}
+                date="2017 - 2019"
+                iconStyle={{ background: '#1ccebf', color: '#000' }}
                 icon={<>p</>}
               >
-                <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                      <h3 className="vertical-timeline-element-title text-black font-bold text-2xl">Lagash 2017 - 2019</h3>
+                      <h4 className="vertical-timeline-element-subtitle font-bold text-xl">Frontend Technical Lead</h4>
                 <p>
-                  User Experience, Visual Design
+                Frontend Technical Lead, Project Management, Team Leading
                 </p>
               </VerticalTimelineElement>
               <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2006 - 2008"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                contentStyle={{ background: '#1ccebf', color: '#000' }}
+                date="2015 - 2017"
+                iconStyle={{ background: '#1ccebf', color: '#000' }}
                 icon={<>p</>}
               >
-                <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                      <h3 className="vertical-timeline-element-title text-black font-bold text-2xl">Lagash 2015 - 2017</h3>
+                      <h4 className="vertical-timeline-element-subtitle font-bold text-xl">Frontend Senior Developer</h4>
                 <p>
-                  User Experience, Visual Design
+            Wip
                 </p>
               </VerticalTimelineElement>
               <VerticalTimelineElement
-                className="vertical-timeline-element--education"
-                date="April 2013"
-                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: '#1ccebf', color: '#000' }}
+                date="2005 - 2015"
+                iconStyle={{ background: '#1ccebf', color: '#000' }}
                 icon={<>p</>}
               >
-                <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-                <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+                      <h3 className="vertical-timeline-element-title text-black font-bold text-2xl">Target 2005 - 2015</h3>
+                      <h4 className="vertical-timeline-element-subtitle font-bold text-xl">Frontend Senior Developer</h4>
+                   
                 <p>
-                  Strategy, Social Media
+            Wip
                 </p>
               </VerticalTimelineElement>
-              <VerticalTimelineElement
-                className="vertical-timeline-element--education"
-                date="November 2012"
-                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                icon={<>p</>}
-              >
-                <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-                <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-                <p>
-                  Creative Direction, User Experience, Visual Design
-                </p>
-              </VerticalTimelineElement>
-              <VerticalTimelineElement
-                className="vertical-timeline-element--education"
-                date="2002 - 2006"
-                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                icon={<>p</>}
-              >
-                <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-                <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-                <p>
-                  Creative Direction, Visual Design
-                </p>
-              </VerticalTimelineElement>
-              <VerticalTimelineElement
-                iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-                icon={<>p</>}
-              />
+              
           </VerticalTimeline>
         </div>
       </div>
